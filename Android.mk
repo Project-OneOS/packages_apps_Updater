c04811fdaf527d04d032461dd213574a6b0a3385
IPSBHANGU <inderpreetbhangu2@gmail.com>
2020-08-03 00:08:56 +0530

    Rebrand to OneOS

--- Android.mk
+++ Android.mk
@@ -20,17 +20,17 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res
 
-LOCAL_PACKAGE_NAME := Updates
+LOCAL_PACKAGE_NAME := Updater 
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
-LOCAL_REQUIRED_MODULES := privapp_whitelist_org.evolution.ota.xml
+LOCAL_REQUIRED_MODULES := privapp_whitelist_com.teamone.updater.xml
 
 include $(BUILD_PACKAGE)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := privapp_whitelist_org.evolution.ota.xml
+LOCAL_MODULE := privapp_whitelist_com.teamone.updater.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
@@ -38,7 +38,7 @@ LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := UpdatesStudio
+LOCAL_MODULE := UpdaterStudio
 LOCAL_MODULE_CLASS := FAKE
 LOCAL_MODULE_SUFFIX := -timestamp
 updater_system_deps := $(call java-lib-deps,framework)
